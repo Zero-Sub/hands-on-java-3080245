c328b1e8a70f87a194aaf1d790d6a02c1a8c34ba
George V <g.vaneaton@icloud.com>
2023-04-19 20:56:33 +0000

    add connection

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -21,13 +23,63 @@ public class DataSource {
 
     return connection;
     
+  }
+
+  public static Customer getCustomer(String username){
+
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
 
 
+    try(Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+
+          statement.setString(1, username);
+          try(ResultSet resultSet = statement.executeQuery()){
+              customer = new Customer(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("username"),
+                resultSet.getString("password"),
+                resultSet.getInt("account_id"));
+              
+          }
+          
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+    return customer;
+  }
+
+  public static Account getAccount(int accountid) {
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+
+    try(Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+          statement.setInt(1, accountid);
+          try(ResultSet resultSet = statement.executeQuery()){
+            account = new Account(
+              resultSet.getInt("id"),
+              resultSet.getString("type"),
+              resultSet.getDouble("balance"));
+          }
+        }catch(SQLException e){
+          e.printStackTrace();
+        }
+
+return account;
   }
 
   public static void main(String[] args){
 
-    connect();
+    //connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    Account account = getAccount(customer.getAccoutId());
     
+    System.out.println(customer.getName());
+    System.out.println(account.getBalance());
+
   }
+
 }
